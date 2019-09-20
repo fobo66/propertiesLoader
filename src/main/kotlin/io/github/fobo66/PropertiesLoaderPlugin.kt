c8377524a7a6a6098adfc6a2c15bdde39945f7f1
Andrey Mukamolov <fobo66@protonmail.com>
2019-09-20 22:09:20 +0300

    Added basic code for loading properties

--- src/main/kotlin/io/github/fobo66/PropertiesLoaderPlugin.kt
+++ src/main/kotlin/io/github/fobo66/PropertiesLoaderPlugin.kt
@@ -2,13 +2,31 @@ package io.github.fobo66
 
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.file.FileCollection
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.InputFiles
+import java.io.FileInputStream
+import java.util.Properties
+import javax.inject.Inject
 
-class PropertiesLoaderPlugin: Plugin<Project> {
-    override fun apply(project: Project) {
-        project.tasks.register("loadProperties") { task ->
-            task.doLast {
-                println("Hello from plugin 'io.github.fobo66.propertiesloader'")
-            }
-        }
+class PropertiesLoaderPlugin @Inject constructor(objects: ObjectFactory) : Plugin<Project> {
+
+  @InputFiles
+  val propertiesFiles: FileCollection = objects.fileCollection()
+
+  override fun apply(project: Project) {
+    project.tasks.register("loadProperties") { task ->
+      task.doFirst {
+        it.logger.quiet("Starting loading properties from files")
+        println("Hello from plugin 'io.github.fobo66.propertiesloader'")
+      }
     }
+  }
+
+  fun loadProperties() {
+    propertiesFiles.files.forEach {
+      val properties = Properties()
+      properties.load(FileInputStream(it))
+    }
+  }
 }
