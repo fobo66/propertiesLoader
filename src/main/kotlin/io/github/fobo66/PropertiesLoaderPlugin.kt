6dbc1bd67e81b4dc43d796345e6951457068be15
Andrey Mukamolov <fobo66@protonmail.com>
2019-09-22 14:04:25 +0300

    Filter files by .properties extension

--- src/main/kotlin/io/github/fobo66/PropertiesLoaderPlugin.kt
+++ src/main/kotlin/io/github/fobo66/PropertiesLoaderPlugin.kt
@@ -7,36 +7,36 @@ import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.api.tasks.InputFiles
 import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.util.Properties
 import javax.inject.Inject
 
 class PropertiesLoaderPlugin @Inject constructor(objects: ObjectFactory) : Plugin<Project> {
 
-  @InputFiles
-  val propertiesFiles: FileCollection = objects.fileCollection()
+    @InputFiles
+    val propertiesFiles: FileCollection = objects.fileCollection()
 
-  override fun apply(project: Project) {
-    project.tasks.register("loadProperties") { task ->
-      task.doFirst {
-        it.logger.quiet("Starting loading properties from files")
-        println("Hello from plugin 'io.github.fobo66.propertiesloader'")
-      }
+    override fun apply(project: Project) {
+        project.tasks.register("loadProperties") { task ->
+            task.doFirst {
+                it.logger.quiet("Starting loading properties from files")
+                println("Hello from plugin 'io.github.fobo66.propertiesloader'")
+            }
+        }
     }
-  }
 
-  fun loadProperties(extras: ExtraPropertiesExtension) {
-    propertiesFiles.files.stream()
-            .map {
-                val properties = Properties()
-                properties.load(FileInputStream(it))
-                return@map properties
-            }
-            .flatMap {
-                return@flatMap it.entries.stream()
-            }
-            .forEach {
-                extras.set(it.component1().toString(), it.component2())
-            }
-  }
+    fun loadProperties(extras: ExtraPropertiesExtension) {
+        propertiesFiles.files.stream()
+                .filter { file -> file.name.endsWith(".properties") }
+                .map {
+                    val properties = Properties()
+                    properties.load(FileInputStream(it))
+                    return@map properties
+                }
+                .flatMap {
+                    return@flatMap it.entries.stream()
+                }
+                .forEach {
+                    extras.set(it.component1().toString(), it.component2())
+                }
+    }
 }
