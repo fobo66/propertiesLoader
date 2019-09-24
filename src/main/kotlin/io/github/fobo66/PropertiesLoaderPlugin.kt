55e84269e5c74f4654a1caea4a082629743e94df
Andrey Mukamolov <amv@bamboogroup.eu>
2019-09-24 10:32:05 +0300

    Tweaked task

--- src/main/kotlin/io/github/fobo66/PropertiesLoaderPlugin.kt
+++ src/main/kotlin/io/github/fobo66/PropertiesLoaderPlugin.kt
@@ -8,9 +8,10 @@ class PropertiesLoaderPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val extension = project.extensions.create("propertiesLoader", PropertiesLoaderExtension::class.java, project)
 
-        project.tasks.register("loadProperties") { task ->
-            task.doFirst {
-                it.logger.quiet("Starting loading properties from files")
+        project.tasks.register<PropertiesLoaderTask>("loadProperties", PropertiesLoaderTask::class.java) { task ->
+            task.propertiesFiles.from(extension.propertiesFiles)
+
+            task.doLast {
                 println("Hello from plugin 'io.github.fobo66.propertiesloader'")
             }
         }
--- src/main/kotlin/io/github/fobo66/PropertiesLoaderTask.kt
+++ src/main/kotlin/io/github/fobo66/PropertiesLoaderTask.kt
@@ -1,17 +1,17 @@
 package io.github.fobo66
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.file.FileCollection
+import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.TaskAction
 import java.io.FileInputStream
 import java.util.*
 
-class PropertiesLoaderTask @javax.inject.Inject constructor(objects: ObjectFactory) : DefaultTask() {
+open class PropertiesLoaderTask @javax.inject.Inject constructor(objects: ObjectFactory) : DefaultTask() {
 
-    @InputFiles
-    val propertiesFiles: FileCollection = objects.fileCollection()
+    @get:InputFiles
+    val propertiesFiles : ConfigurableFileCollection = objects.fileCollection()
 
     @TaskAction
     fun loadProperties() {
