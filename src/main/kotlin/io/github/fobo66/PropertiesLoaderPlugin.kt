f905a43bc17b2f43894695a0fd2daa5d862342e7
Andrey Mukamolov <fobo66@protonmail.com>
2019-10-02 10:30:37 +0300

    Make task run before all other tasks

--- src/main/kotlin/io/github/fobo66/PropertiesLoaderPlugin.kt
+++ src/main/kotlin/io/github/fobo66/PropertiesLoaderPlugin.kt
@@ -9,7 +9,13 @@ class PropertiesLoaderPlugin : Plugin<Project> {
         val extension = project.extensions.create("propertiesLoader", PropertiesLoaderExtension::class.java, project)
 
         project.tasks.register<PropertiesLoaderTask>("loadProperties", PropertiesLoaderTask::class.java) { task ->
+            task.description = "Load properties from file to project extras"
             task.propertiesFiles.from(extension.propertiesFiles)
         }
+
+        project.tasks.matching { task -> task !is PropertiesLoaderTask }
+                .all { task ->
+                    task.dependsOn(":loadProperties")
+                }
     }
 }
