5ffbbc19e2453f1c25a4a4a42326aea0b462bdab
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-11-16 23:28:14 +0200

    Fixed some Detekt complaints

--- src/main/kotlin/io/github/fobo66/PropertiesLoaderTask.kt
+++ src/main/kotlin/io/github/fobo66/PropertiesLoaderTask.kt
@@ -2,12 +2,9 @@ package io.github.fobo66
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.*
-import org.gradle.work.InputChanges
 import java.io.FileInputStream
 import java.util.*
-import javax.inject.Inject
 
 @CacheableTask
 abstract class PropertiesLoaderTask : DefaultTask() {
@@ -21,20 +18,20 @@ abstract class PropertiesLoaderTask : DefaultTask() {
     fun loadProperties() {
         logger.debug("Starting loading properties")
         propertiesFiles.files.stream()
-                .filter { file -> file.name.endsWith(".properties") }
-                .map {
-                    logger.debug("Trying to process properties file ${it.name}")
-                    val properties = Properties()
-                    properties.load(FileInputStream(it))
-                    return@map properties
-                }
-                .flatMap {
-                    logger.debug("Switching to properties' entries")
-                    return@flatMap it.entries.stream()
-                }
-                .forEach { (key, value) ->
-                    logger.debug("Adding property to extras")
-                    project.extensions.extraProperties.set(key.toString(), value)
-                }
+            .filter { file -> file.name.endsWith(".properties") }
+            .map {
+                logger.debug("Trying to process properties file ${it.name}")
+                val properties = Properties()
+                properties.load(FileInputStream(it))
+                return@map properties
+            }
+            .flatMap {
+                logger.debug("Switching to properties' entries")
+                return@flatMap it.entries.stream()
+            }
+            .forEach { (key, value) ->
+                logger.debug("Adding property to extras")
+                project.extensions.extraProperties.set(key.toString(), value)
+            }
     }
 }
