b36796b1907ff9c026a9ae23c76e5e5821a0ae5b
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2023-01-04 22:20:22 +0200

    Modernized DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,40 +13,23 @@ repositories {
 version = "1.1"
 group = "io.github.fobo66.propertiesloader"
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit"))
-}
-
-pluginBundle {
-    website = "https://github.com/fobo66/propertiesLoader"
-    vcsUrl = "https://github.com/fobo66/propertiesLoader.git"
-    tags = listOf("properties", "config", "extras")
-}
-
-gradlePlugin {
-    plugins.register("propertiesLoader") {
-        id = "io.github.fobo66.propertiesloader"
-        displayName = "PropertiesLoader – load data from .properties files into project extras"
-        description = """A plugin that helps you with loading sensitive data like API keys from .properties files into project extras,
-            | so you can use these sensitive keys right from ext
-        """.trimMargin()
-        implementationClass = "io.github.fobo66.PropertiesLoaderPlugin"
+kotlin {
+    jvmToolchain {
+        version = 17
     }
 }
 
-val functionalTestSourceSet = sourceSets.create("functionalTest") {
-}
-
-gradlePlugin.testSourceSets(functionalTestSourceSet)
-configurations.getByName("functionalTestImplementation").extendsFrom(configurations.getByName("testImplementation"))
-
-val functionalTest by tasks.creating(Test::class) {
-    testClassesDirs = functionalTestSourceSet.output.classesDirs
-    classpath = functionalTestSourceSet.runtimeClasspath
-}
-
-val check by tasks.getting(Task::class) {
-    dependsOn(functionalTest)
+gradlePlugin {
+    website.set("https://github.com/fobo66/propertiesLoader")
+    vcsUrl.set("https://github.com/fobo66/propertiesLoader.git")
+    plugins.register("propertiesLoader") {
+        id = "io.github.fobo66.propertiesloader"
+        displayName = "PropertiesLoader – load data from .properties files into project extras"
+        description =
+            """A plugin that helps you with loading sensitive data like API keys from .properties files into project extras,
+            | so you can use these sensitive keys right from ext
+        """.trimMargin()
+        tags.set(listOf("properties", "config", "extras"))
+        implementationClass = "io.github.fobo66.PropertiesLoaderPlugin"
+    }
 }
